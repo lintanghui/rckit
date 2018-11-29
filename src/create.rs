86923d710254d82191d9e697356bc8d6558857ca
lintanghui <lintanghui@bilibili.com>
2018-11-29 11:57:22 +0800

    distribute slave

--- src/create.rs
+++ src/create.rs
@@ -10,15 +10,20 @@ fn test_cluster() {
     addrs.push(b"127.0.0.2:8888");
     addrs.push(b"127.0.0.3:8888");
     addrs.push(b"127.0.0.4:8888");
-    addrs.push(b"127.0.0.5:8888");
-    addrs.push(b"127.0.0.6:8888");
-    addrs.push(b"127.0.0.7:8888");
-    addrs.push(b"127.0.0.8:8888");
+    addrs.push(b"127.0.0.1:8889");
+    addrs.push(b"127.0.0.2:8889");
+    addrs.push(b"127.0.0.3:8889");
+    addrs.push(b"127.0.0.4:8889");
 
     let mut cluster = Cluster::new(addrs, 4, 4).unwrap();
     cluster.init_slots();
     assert_eq!(cluster.master.len(), 4);
     assert_eq!(cluster.slots.len(), 4);
+    assert_eq!(cluster.slave.len(), 4);
+    println!("{:?}", cluster.slave.pop());
+    println!("{:?}", cluster.slave.pop());
+    println!("{:?}", cluster.slave.pop());
+    println!("{:?}", cluster.slave.pop());
 }
 #[test]
 fn test_split_slots() {
@@ -68,7 +73,7 @@ fn test_spread() {
     };
     map.insert("11", vec![node1, node2]);
     map.insert("13", vec![node3, node4]);
-    let mut target = spread(&map, 3).unwrap();
+    let mut target = spread(&mut map, 3).unwrap();
     assert_eq!(target.len(), 3);
     println!("{:?}", target.pop());
     println!("{:?}", target.pop());
@@ -119,13 +124,16 @@ impl Cluster {
         }
     }
     pub fn init_slots(&mut self) {
-        let mut ips = HashMap::new();
-        for n in &self.nodes {
-            ips.entry(key.clone()).or_insert(vec![]).push(n.clone());
-        }
-        self.master = spread(&ips, self.master_count).unwrap();
-        self.slots = slpit_slots(CLUSTER_SLOTS, self.master_count).unwrap();
-        let slaves = spread(&ips, self.nodes.len() - self.master_count).unwrap();
+        let slaves = {
+            let mut ips = HashMap::new();
+            for n in &self.nodes {
+                let key = &*n.ip;
+                ips.entry(key).or_insert(vec![]).push(n.clone());
+            }
+            self.master = spread(&mut ips, self.master_count).unwrap();
+            self.slots = slpit_slots(CLUSTER_SLOTS, self.master_count).unwrap();
+            spread(&mut ips, self.nodes.len() - self.master_count).unwrap()
+        };
         self.distribute_slave(slaves);
     }
 
@@ -137,19 +145,23 @@ impl Cluster {
                     if master.ip == slave.ip {
                         continue;
                     }
-                    let mut key = String::from(slave.ip.clone());
+                    let mut key = String::from(slave.ip.clone() + ":" + &slave.port);
                     if inuse.contains_key(&key) {
                         continue;
                     }
                     inuse.insert(key, slave);
-                    let mut slaveof = String::from(master.ip.clone() + &master.port);
+                    let mut slaveof = String::from(master.ip.clone() + ":" + &master.port);
                     let mut s = Node {
                         ip: slave.ip.clone(),
                         port: slave.port.clone(),
                         slave: Some(slaveof),
                     };
                     self.slave.push(s);
+                    break;
                 }
+            }
+            if self.slave.len() >= slaves.len() {
+                break;
             }
         }
     }
@@ -210,12 +222,22 @@ fn divide(n: usize, m: usize) -> Vec<usize> {
     c
 }
 
-fn spread(nodes: &HashMap<&str, Vec<Node>>, n: usize) -> Option<Vec<Node>> {
-    let target = nodes
-        .into_iter()
-        .map(|(_, v)| v.clone())
-        .flatten()
-        .take(n)
-        .collect();
-    Some(target)
+fn spread(nodes: &mut HashMap<&str, Vec<Node>>, n: usize) -> Option<Vec<Node>> {
+    let mut target: Vec<Node> = Vec::new();
+    loop {
+        for (_, v) in nodes.into_iter() {
+            if target.len() >= n {
+                return Some(target);
+            }
+            let node = v.pop().unwrap();
+            target.push(node);
+        }
+    }
+    // let target = nodes
+    //     .into_iter()
+    //     .map(|(_, v)| v.clone())
+    //     .flatten()
+    //     .take(n)
+    //     .collect();
+    // Some(target)
 }
