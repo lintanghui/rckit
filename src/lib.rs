a35c5d703d1b2e62514e7fb091d14a584169bf73
wayslog <zxs867179@gmail.com>
2018-12-27 18:38:09 +0800

    add cli.yml to split the config into config file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 #![allow(clippy::let_unit_value)]
+#[macro_use]
 extern crate clap;
 extern crate redis;
 
@@ -9,108 +10,15 @@ mod create;
 mod util;
 
 use add::Add;
-use clap::{App, Arg, SubCommand};
+use clap::App;
 use cluster::{Cluster, Node};
 use create::Create;
 use std::{thread, time};
 
 pub fn run() {
 
-    let matches = App::new("rckit")
-        .about("redis cluster tool")
-        .subcommand(
-            SubCommand::with_name("create")
-                .about("create rredis cluster")
-                .arg(
-                    Arg::with_name("node")
-                        .help("cluster nodes")
-                        .short("n")
-                        .required(true)
-                        .multiple(true)
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("replicate")
-                        .short("s")
-                        .default_value("1")
-                        .help("slave replicate number "),
-                )
-                .arg(
-                    Arg::with_name("master")
-                        .short("m")
-                        .default_value("0")
-                        .takes_value(true)
-                        .help("mster number"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("add")
-                .about("Add  node to existing cluster")
-                .arg(
-                    Arg::with_name("cluster")
-                        .required(true)
-                        .short("c")
-                        .help("-c clusterip:port, spec cluster ip and  port")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("node")
-                        .required(true)
-                        .short("n")
-                        .help(
-                            "add new node  to cluster,
-                        -n <master,slave> <master>",
-                        )
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("delete")
-                .about(
-                    "delete node from cluster.\r\nif node is a master,it will migrate slots to other node and delete is's slave too",
-                )
-                .arg(
-                    Arg::with_name("node")
-                        .help("-n <node>")
-                        .short("n")
-                        .required(true)
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("migrate")
-            .about("migrate slots from src to dst")
-            .arg(
-                Arg::with_name("dst")
-                    .short("d")
-                    .help("-d <node>")
-                    .takes_value(true),
-                )
-            .arg(
-                Arg::with_name("src")
-                .short("s")
-                .help("-s <node>")
-                .takes_value(true),
-                )
-            .arg(
-                Arg::with_name("count")
-                .help("-c count")
-                .short("c")
-                .takes_value(true),
-            ),
-        )
-        .subcommand(
-            SubCommand::with_name("fix").
-            about("fix cluster")
-            .arg(
-                Arg::with_name("node").
-                short("n").
-                required(true).
-                takes_value(true),
-            ),
-        ).subcommand(SubCommand::with_name("reshard").about("rebalance slots").arg(Arg::with_name("node")
-        .short("n").required(true).takes_value(true)))
-        .get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(sub_m) = matches.subcommand_matches("create") {
         let slave_count = clap::value_t!(sub_m.value_of("replicate"), usize).unwrap();
